4fb10571aab7e4a5854511b93e843c40bd636dbd
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-12-06 12:18:12 +0100

    header commencé

---
+++ wp-content/themes/pkm/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon Thème</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- my style -->
+    <link rel="stylesheet" href="/wp-content/themes/pkm/public/assets/css/style.css">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Lato&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+    <!-- ! Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="/wp-content/themes/pkm/public/assets/img/pokemon-logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link px-4" href="#">Catégorie 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-4" href="#">Catégorie 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-4" href="#">Catégorie 3</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- ! Cover -->
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col" id="cover__img">
+
+            </div>
+        </div>
+    </section>
+</header>
+
+
+
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
