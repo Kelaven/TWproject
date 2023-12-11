ca6f8b9cb873c6858f4d7ad77cf8ab5d29d15682
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-12-11 10:27:35 +0100

    page d'accueil ok

--- wp-content/themes/pkm/header.php
+++ wp-content/themes/pkm/header.php
@@ -16,7 +16,7 @@
         <!-- ! Navbar -->
         <nav class="navbar navbar-expand-lg bg-body-tertiary px-lg-5 mx-lg-5">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="/wp-content/themes/pkm/public/assets/img/pokemon-logo.png" alt="logo pokemon theme wordpress"></a>
+                <a class="navbar-brand" href="/"><img src="/wp-content/themes/pkm/public/assets/img/pokemon-logo.png" alt="logo pokemon theme wordpress"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- wp-content/themes/pkm/index.php
+++ wp-content/themes/pkm/index.php
@@ -17,6 +17,14 @@
         <div class="row">
             <div class="col" id="main__content">
 
+                <!-- ! contenu de la page d'accueil -->
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                    <?php the_content(); ?>
+
+                <?php endwhile;
+                endif; ?>
+
             </div>
         </div>
     </section>
